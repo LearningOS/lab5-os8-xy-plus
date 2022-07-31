306be958aaedb994df56c651b8bb5e94c099c48d
xy-plus <40733434+xy-plus@users.noreply.github.com>
2022-07-31 08:33:53 +0000

    fmt

--- os8/src/fs/mod.rs
+++ os8/src/fs/mod.rs
@@ -1,11 +1,11 @@
-mod stdio;
 mod inode;
 mod pipe;
+mod stdio;
 
 use crate::mm::UserBuffer;
 
 /// The common abstraction of all IO resources
-pub trait File : Send + Sync {
+pub trait File: Send + Sync {
     fn readable(&self) -> bool;
     fn writable(&self) -> bool;
     fn read(&self, buf: UserBuffer) -> usize;
@@ -38,8 +38,8 @@ bitflags! {
         /// ordinary regular file
         const FILE  = 0o100000;
     }
-}    
+}
 
+pub use inode::{list_apps, open_file, OSInode, OpenFlags};
+pub use pipe::{make_pipe, Pipe};
 pub use stdio::{Stdin, Stdout};
-pub use inode::{OSInode, open_file, OpenFlags, list_apps};
-pub use pipe::{Pipe, make_pipe};
--- os8/src/mm/mod.rs
+++ os8/src/mm/mod.rs
@@ -6,7 +6,6 @@
 //!
 //! Every task or process has a memory_set to control its virtual memory.
 
-
 mod address;
 mod frame_allocator;
 mod heap_allocator;
@@ -16,9 +15,11 @@ mod page_table;
 pub use address::{PhysAddr, PhysPageNum, VirtAddr, VirtPageNum};
 pub use address::{StepByOne, VPNRange};
 pub use frame_allocator::{frame_alloc, frame_dealloc, FrameTracker};
-pub use memory_set::{remap_test, kernel_token};
+pub use memory_set::{kernel_token, remap_test};
 pub use memory_set::{MapPermission, MemorySet, KERNEL_SPACE};
-pub use page_table::{translated_byte_buffer, translated_refmut, translated_ref, translated_str, PageTableEntry};
+pub use page_table::{
+    translated_byte_buffer, translated_ref, translated_refmut, translated_str, PageTableEntry,
+};
 pub use page_table::{PTEFlags, PageTable, UserBuffer};
 
 /// initiate heap allocator, frame allocator and kernel space
